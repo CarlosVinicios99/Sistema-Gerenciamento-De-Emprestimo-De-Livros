a486f634d5e74250ea77a24dbd28c890a0701d0c
carlosvinicios <carlosvinicios@id.uff.br>
2022-12-20 03:06:39 -0300

    Classe Livro com primeiros atributos definidos

--- sistema-de-emprestimo-de-livros/src/model/entities/Livro.java
+++ sistema-de-emprestimo-de-livros/src/model/entities/Livro.java
@@ -1,5 +1,17 @@
 package model.entities;
 
-public class Livro {
+import java.util.List;
 
+public class Livro {
+	
+	private int id;
+	
+	private String titulo;
+	
+	private String descricao;
+	
+	private String autor;
+		
+	//private List<Exemplar> exemplares = new Exemplar();
+	
 }
