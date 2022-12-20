e60d01a117c8016b0252c4a7f74b330e8b891e9d
carlosvinicios <carlosvinicios@id.uff.br>
2022-12-20 02:37:37 -0300

    primeiros atributos e metodos definidos na classe Usuario

--- sistema-de-emprestimo-de-livros/src/model/entities/Usuario.java
+++ sistema-de-emprestimo-de-livros/src/model/entities/Usuario.java
@@ -1,5 +1,53 @@
 package model.entities;
 
-public class Usuario {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Usuario {
+	
+	private int id;
+	
+	private String matricula;
+	
+	private String nome;
+	
+	private String cpf;
+	
+	private String email;
+	
+	private String senha;
+	
+	private List<Emprestimo> emprestimos = new ArrayList<Emprestimo>();
+	
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario(String matricula, String nome, String cpf, String email, String senha) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	public boolean adicionarEmprestimo(Emprestimo emprestimo) {
+		if(emprestimos.size() == 3 || emprestimo == null) {
+			return false;
+		}
+		return emprestimos.add(emprestimo);
+	}
+	
+	public boolean removerEmprestimo(Emprestimo emprestimo) {
+		return emprestimos.remove(emprestimo);
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "\nID: " + id + "\nMatricula: " + matricula + "\nNome: " + nome
+			+ "\nCPF: " + cpf + "\nE-mail: " + email + "\nSenha: " + senha;
+	}
+	
 }
