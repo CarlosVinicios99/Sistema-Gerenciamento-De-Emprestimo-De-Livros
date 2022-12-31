3aafeb1a46e38d4764f84fb6620afda8a6caa24a
Carlos Vinicios <carlosvinicios@id.uff.br>
2022-12-31 19:35:54 -0300

    Implementacao da tela inicial e do posicionamento da tela de login

--- sistema-de-emprestimo-de-livros/src/app/view/TelaInicial.java
+++ sistema-de-emprestimo-de-livros/src/app/view/TelaInicial.java
@@ -1,6 +1,8 @@
 package app.view;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -10,6 +12,8 @@ public class TelaInicial {
 	private Stage secondStage = new Stage();
 	private Scene cenaInicial;
 	private String CSS = getClass().getResource("/app/view/estilo.css").toExternalForm();
+	private HBox boxTelaInicial = new HBox();
+	private Label labelTitulo = new Label("Universidade Federal Fluminense");
 	
 	public TelaInicial(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -18,7 +22,13 @@ public class TelaInicial {
 	}
 	
 	private void inicializar() {
-		cenaInicial = new Scene(new HBox(), 1200, 650);
+		labelTitulo.getStyleClass().add("tituloPrincipal");
+		boxTelaInicial.setSpacing(30);
+		boxTelaInicial.setAlignment(Pos.BASELINE_CENTER);
+		boxTelaInicial.getChildren().add(labelTitulo);
+		boxTelaInicial.getStyleClass().add("telaInicial");
+		cenaInicial = new Scene(boxTelaInicial, 1200, 650);
+		cenaInicial.getStylesheets().add(CSS);
 		primaryStage.setScene(cenaInicial);
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("Emprestimo De Livros");
@@ -28,6 +38,8 @@ public class TelaInicial {
 	private void exibirTelaDeLogin() {
 		Scene login = TelaDeLogin.obterCena();
 		secondStage.setScene(login);
+		secondStage.setX(560);
+		secondStage.setY(200);
 		secondStage.setResizable(false);
 		secondStage.setTitle("Login");
 		secondStage.show();
