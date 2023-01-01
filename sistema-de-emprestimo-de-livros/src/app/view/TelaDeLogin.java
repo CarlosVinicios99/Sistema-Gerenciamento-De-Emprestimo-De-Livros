a4e2012264dc961c997e234a86065e468856493d
Carlos Vinicios <carlosvinicios@id.uff.br>
2022-12-31 21:24:46 -0300

    Layout da tela de login definido, correcoes a serem feitas no CSS

--- sistema-de-emprestimo-de-livros/src/app/view/TelaDeLogin.java
+++ sistema-de-emprestimo-de-livros/src/app/view/TelaDeLogin.java
@@ -7,11 +7,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class TelaDeLogin {
 	
-	static private HBox boxLogin;
+	static private GridPane gridPaneLogin;
 	static private Scene cenaLogin;
 	static private Label labelEmail;
 	static private Label labelSenha;
@@ -24,42 +26,64 @@ public class TelaDeLogin {
 	}
 	
 	private static void configurarElementosVisuais() {
-		boxLogin = new HBox();
-		boxLogin.setMaxHeight(250);
-		boxLogin.setMaxWidth(400);
-		boxLogin.setAlignment(Pos.BASELINE_CENTER);
-		boxLogin.setSpacing(10);
+		gridPaneLogin = new GridPane();
+		criarGrid();
 		
-		labelEmail = new Label("E-mail:");
-		labelEmail.setMaxSize(30, 15);
-		labelSenha = new Label("Senha:");
+		labelEmail = new Label("E-mail: ");
+		labelEmail.setMaxSize(70, 25);
+		labelEmail.setAlignment(Pos.BASELINE_RIGHT);
+		labelSenha = new Label("Senha: ");
+		labelSenha.setMaxSize(70, 25);
+		labelSenha.setAlignment(Pos.BASELINE_RIGHT);
 		
 		campoEmail = new TextField();
+		campoEmail.setMaxSize(230, 30);
 		campoSenha = new PasswordField();
+		campoSenha.setMaxSize(230, 30);
 		
 		botaoEntrar = new Button("Entrar");
+		botaoEntrar.setMaxSize(70, 25);
+		
 	}
 	
 	private static void adicionarElementosNaTela() {
-		boxLogin.getChildren().add(labelEmail);
-		boxLogin.getChildren().add(campoEmail);
-		boxLogin.getChildren().add(labelSenha);
-		boxLogin.getChildren().add(campoSenha);
-		boxLogin.getChildren().add(botaoEntrar);
+		gridPaneLogin.add(labelEmail, 0, 0);
+		gridPaneLogin.add(campoEmail, 1, 0);
+		gridPaneLogin.add(labelSenha, 0, 1);
+		gridPaneLogin.add(campoSenha, 1, 1);
+		gridPaneLogin.add(botaoEntrar, 0, 2);
 	}
 	
 	private static void adicionarAcaoBotaoEntrar() {
 		
 	}
 	
+	private static RowConstraints criarLinha() {
+		RowConstraints linha = new RowConstraints();
+		linha.setPercentHeight(20);
+		linha.setFillHeight(true);
+		return linha;
+	}
+	
+	private static ColumnConstraints criarColuna(double width) {
+		ColumnConstraints coluna = new ColumnConstraints();
+		coluna.setFillWidth(true);
+		coluna.setPercentWidth(width);
+		return coluna;
+	}
+	
+	private static void criarGrid() {
+		gridPaneLogin.getRowConstraints().addAll(criarLinha(), criarLinha(), criarLinha(), criarLinha(), criarLinha());
+		gridPaneLogin.getColumnConstraints().addAll(criarColuna(25), criarColuna(75));
+	}
+	
+	
 	public static Scene obterCena() {
 		configurarElementosVisuais();
 		adicionarElementosNaTela();
 		adicionarAcaoBotaoEntrar();
-		cenaLogin = new Scene(boxLogin, 300, 400);
+		cenaLogin = new Scene(gridPaneLogin, 350, 400);
 		return cenaLogin;
 	}
-	
-	
 	
 }
