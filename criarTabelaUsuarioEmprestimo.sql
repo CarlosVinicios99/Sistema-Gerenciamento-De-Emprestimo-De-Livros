939f8c79377aebde52bfae5144add3bb1817a9de
Carlos Vinicios De Souza Azeredo <55165185+CarlosVinicios99@users.noreply.github.com>
2023-01-07 03:09:17 -0300

    Delete criarTabelaUsuarioEmprestimo.sql

--- criarTabelaUsuarioEmprestimo.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuario_emprestimo(
-	id INT NOT NULL AUTO_INCREMENT,
-	id_usuario INT NOT NULL,
-    id_emprestimo INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_usuario) REFERENCES usuarios(id),
-    FOREIGN KEY(id_emprestimo) REFERENCES emprestimos(id),
-    UNIQUE KEY(id_emprestimo)
-);
